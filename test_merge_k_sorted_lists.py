12ed4f196099aab329afcd7bd3107b1d80821a7f
Brigitte Unger <brigitteunger@users.noreply.github.com>
2020-05-26 20:06:14 +0200

    new kata merge n sorted linked lists

---
+++ test_merge_k_sorted_lists.py
@@ -0,0 +1,195 @@
+import unittest
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution():
+    def merge_k_lists(self, lists: List[ListNode]) -> ListNode:
+        lists = [item for item in lists if item is not None]
+
+        if not lists:
+            return None
+
+        index_smallest = self.find_smallest_node(lists)
+        head = lists[index_smallest]
+
+        if lists[index_smallest].next is None:
+            del lists[index_smallest]
+        else:
+            lists[index_smallest] = lists[index_smallest].next
+
+        node = head
+        while 1:
+            if not lists:
+                break
+            index_smallest = self.find_smallest_node(lists)
+            smallest_node = lists[index_smallest]
+            node.next = smallest_node
+            node = node.next
+
+            if lists[index_smallest].next is None:
+                del lists[index_smallest]
+            else:
+                lists[index_smallest] = lists[index_smallest].next
+
+        return head
+
+    def find_smallest_node(self, lists: List[ListNode]) -> int:
+        smallest_value = lists[0].val
+        index_smallest_node = 0
+        for index in range(1, len(lists)):
+            node = lists[index]
+            if node.val < smallest_value:
+                smallest_value = node.val
+                index_smallest_node = index
+        return index_smallest_node
+
+
+class TestListNode(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def convert_list_2_linked_list(self, vals: list) -> ListNode:
+        node = None
+        for val in reversed(vals):
+            node = ListNode(val, node)
+        return node
+
+    def convert_linked_list_2_list(self, node: ListNode) -> list:
+        vals = []
+        while node is not None:
+            vals.append(node.val)
+            node = node.next
+        return vals
+
+    def test_convertion(self):
+        vals = [4, 3, 5]
+
+        linked_vals = self.convert_list_2_linked_list(vals)
+        vals_processed = self.convert_linked_list_2_list(linked_vals)
+
+        self.assertEqual(vals_processed, vals_processed)
+
+    def create_list_linked_lists(self, list_lists: list) -> List[ListNode]:
+        list_linked_lists = []
+        for list_n in list_lists:
+            linked_list = self.convert_list_2_linked_list(list_n)
+            list_linked_lists.append(linked_list)
+        return list_linked_lists
+
+    def test_merge_k_lists(self):
+        list_lists = []
+        list_lists.append([1, 4, 5])
+        list_lists.append([1, 3, 4])
+        list_lists.append([2, 6])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [1, 1, 2, 3, 4, 4, 5, 6]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_all_equal(self):
+        list_lists = []
+        list_lists.append([1, 1, 1])
+        list_lists.append([1, 1, 1])
+        list_lists.append([1, 1, 1])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_two_lists(self):
+        list_lists = []
+        list_lists.append([-5, 1, 4])
+        list_lists.append([1, 3, 4])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [-5, 1, 1, 3, 4, 4]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_four_lists(self):
+        list_lists = []
+        list_lists.append([-30, 0, 0])
+        list_lists.append([-5, 1, 4])
+        list_lists.append([1, 3, 4])
+        list_lists.append([-6, -5, -3, 2, 2, 6, 100])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [-30, -6, -5, -5, -3, 0, 0, 1, 1, 2, 2, 3, 4, 4, 6,
+                         100]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_one_list(self):
+        list_lists = []
+        list_lists.append([1, 4, 5])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [1, 4, 5]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_empty_list(self):
+        list_lists = []
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = []
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_one_empty_list(self):
+        list_lists = []
+        list_lists.append([])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = []
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_one_list_one_element(self):
+        list_lists = []
+        list_lists.append([1])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = [1]
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+    def test_merge_k_lists_two_empty_lists(self):
+        list_lists = []
+        list_lists.append([])
+        list_lists.append([])
+        list_linked_lists = self.create_list_linked_lists(list_lists)
+        merged_result = []
+
+        merged_linked_list = self.sol.merge_k_lists(list_linked_lists)
+        merged_list = self.convert_linked_list_2_list(merged_linked_list)
+
+        self.assertEqual(merged_list, merged_result)
+
+
+test_ = TestListNode()
+test_.test_convertion()
