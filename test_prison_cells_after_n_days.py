5fbda78c078c70a2001b76c801b6f20e071a9818
Brigitte Unger <brigitteunger@users.noreply.github.com>
2020-07-03 12:47:10 +0200

    add kata prison cells after n days

---
+++ test_prison_cells_after_n_days.py
@@ -0,0 +1,75 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
+        cells_after = [0, 0, 0, 0, 0, 0, 0, 0]
+        cell_after_days = {}
+
+        for days in range(1, N+1):
+
+            for m in range(1, 7):
+                if cells[m-1] == cells[m+1]:
+                    cells_after[m] = 1
+                else:
+                    cells_after[m] = 0
+            cells = cells_after[:]
+
+            str_cells = "".join(str(x) for x in cells_after)
+            if str_cells in cell_after_days:
+                num = N % (days-1)  # days-1 = len(cell_after_days)
+                if num == 0:
+                    num = days - 1
+                for key, val in cell_after_days.items():
+                    if val == num:
+                        final_cell = [int(item) for item in key]
+                        return final_cell
+            else:
+                cell_after_days[str_cells] = days
+
+        return cells
+
+
+class TestPrisonAfterNDays(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_prison_after_1_days(self):
+        cells = [0, 1, 0, 1, 1, 0, 0, 1]
+        N = 1
+
+        cells_after = self.sol.prisonAfterNDays(cells, N)
+
+        self.assertEqual(cells_after,
+                         [0, 1, 1, 0, 0, 0, 0, 0])
+
+    def test_prison_after_7_days(self):
+        cells = [0, 1, 0, 1, 1, 0, 0, 1]
+        N = 7
+
+        cells_after = self.sol.prisonAfterNDays(cells, N)
+
+        self.assertEqual(cells_after,
+                         [0, 0, 1, 1, 0, 0, 0, 0])
+
+    def test_prison_after_1000000000_days(self):
+        cells = [1, 0, 0, 1, 0, 0, 1, 0]
+        N = 1000000000
+
+        cells_after = self.sol.prisonAfterNDays(cells, N)
+
+        self.assertEqual(cells_after,
+                         [0, 0, 1, 1, 1, 1, 1, 0])
+
+    def test_prison_after_574_days(self):
+        cells = [0, 0, 0, 1, 1, 0, 1, 0]
+        N = 574
+
+        cells_after = self.sol.prisonAfterNDays(cells, N)
+
+        self.assertEqual(cells_after,
+                         [0, 0, 0, 1, 1, 0, 1, 0])
+
+if __name__ == "__main__":
+    unittest.main()
