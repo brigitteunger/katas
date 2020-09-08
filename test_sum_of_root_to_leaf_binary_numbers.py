152fb2663fe11faf3d0bc844c6f5384eed23adbd
Brigitte Unger <brigitteunger@users.noreply.github.com>
2020-09-08 13:12:36 +0200

    add sum of root to leaf binary numbers

---
+++ test_sum_of_root_to_leaf_binary_numbers.py
@@ -0,0 +1,121 @@
+import unittest
+from typing import Dict, List
+from data_test_width_of_binary_tree import nodes_1
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        return self.sumOverNodes(root, 0)
+
+    def sumOverNodes(self, node: TreeNode, offset: int
+                     ) -> int:
+        if node is None:
+            return 0
+        elif node.left is None and node.right is None:
+            return offset*2 + node.val
+        else:
+            offset = offset*2 + node.val
+            return self.sumOverNodes(node.left, offset) + \
+                self.sumOverNodes(node.right, offset)
+
+
+class TestSumRootToLeaf(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def create_tree_from_list(self, vals: List[int], i: int = 0) -> TreeNode:
+        if i >= len(vals):
+            return None
+        if vals[i] is None:
+            return None
+        return TreeNode(vals[i], self.create_tree_from_list(vals, 2*i+1),
+                        self.create_tree_from_list(vals, 2*i+2))
+
+    def create_list_from_tree(self, root: TreeNode) -> List[int]:
+        nodes = []
+        nodes_to_visit = [root]
+
+        nodes_per_level = 1
+        visited_nodes = 0
+        count_nodes_None = 0
+
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+            if node is None:
+                count_nodes_None += 1
+                nodes_to_visit.append(None)
+                nodes_to_visit.append(None)
+                nodes.append(None)
+            else:
+                nodes.append(node.val)
+                nodes_to_visit.append(node.left)
+                nodes_to_visit.append(node.right)
+
+            visited_nodes += 1
+
+            # one level completed?
+            if nodes_per_level == visited_nodes:
+                if count_nodes_None == nodes_per_level:
+                    nodes = nodes[:len(nodes)-nodes_per_level]
+                    while 1:
+                        if nodes[-1] is None:
+                            del nodes[-1]
+                        else:
+                            break
+                    break
+                else:
+                    nodes_per_level *= 2
+                    count_nodes_None = 0
+                    visited_nodes = 0
+
+        return nodes
+
+    def print_tree_from_list(self, nodes: List[int]):
+        # find number of levels
+        num_nodes = len(nodes)
+        level = 1
+        bigger_level = 2
+        while 1:
+            if num_nodes <= bigger_level-1:
+                break
+            else:
+                bigger_level *= 2
+                level += 1
+
+        printed_nodes = 0
+        space = int(bigger_level)*5
+        string = "{:^" + str(space) + "}"
+        nodes_per_level = 1
+
+        for node in nodes:
+            print(string.format(str(node)), end="")
+            printed_nodes += 1
+            if nodes_per_level == printed_nodes:
+                print()
+                print()
+                nodes_per_level *= 2
+                printed_nodes = 0
+                bigger_level /= 2
+                space = int(bigger_level) * 5
+                string = "{:^"+str(space)+"}"
+        print()
+
+    def testSumRootToLeaf(self):
+        vals = [1, 0, 1, 0, 1, 0, 1]
+        root = self.create_tree_from_list(vals)
+
+        sum_int = self.sol.sumRootToLeaf(root)
+
+        self.assertEqual(sum_int, 22)
+
+
+if __name__ == "__main__":
+    unittest.main()
